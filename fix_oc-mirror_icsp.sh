0791e7060b20a118081f40461807bf105c576403
Koren Peretz <62677694+KorenP1@users.noreply.github.com>
2023-02-21 12:45:53 +0200

    Create fix_oc-mirror_icsp.sh
    init script

---
+++ fix_oc-mirror_icsp.sh
@@ -0,0 +1,27 @@
+echo "What is the name of the operator?"
+read name
+
+cp mapping.txt mapping.txt.new
+
+sed -i "/registry.redhat.io\/redhat\/.*-operator-index:v/d" mapping.txt.new
+sed -i 's/@.*=/=/' mapping.txt.new
+sed -i "s/.\{0,9\}$//; /^$/d" mapping.txt.new
+
+echo "apiVersion: operator.openshift.io/v1alpha1
+kind: ImageContentSourcePolicy
+metadata:
+  name: $name
+spec:
+  repositoryDigestMirrors:" > icsp-new.yaml
+
+while IFS= read -r line
+do
+        IFS== read -r left right <<< `echo $line`
+        echo -e "  - mirrors:
+    - $right
+    source: $left" >> icsp-new.yaml
+done < "mapping.txt.new"
+
+rm -f mapping.txt.new
+
+sed 's/  name:.*/  name: '"$name"'/' catalogSource-*\.yaml > cs-new.yaml
